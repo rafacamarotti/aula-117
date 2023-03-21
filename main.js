ca9c4034be6dd76aef73b5d9b9ef3017facf8172
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-03-21 20:32:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup(){
+    canvas=createCanvas(280, 280);
+    canvas = center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+    classfier = ml5.imageClassifier('DoodleNet');
+}
+function clearcanvas(){
+    background("white");
+}
+function draw(){
+    strokeWeight(13);
+    stroke("black");
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+function classifyCanvas(){
+    classfier.classify(canvas, gotResult);
+}
+function gotResult(error, results){
+if(error){
+    console.error(error);
+}
+console.log(results);
+result=results[0].label;
+document.getElementById('label').innerHTML = 'Nome: '+result.replace('_', ' ');
+document.getElementById('confidence').innerHTML = 'precisão: '+Math.round(results[0].confidence * 100)+ '%';
+utterThis = new SpeechSynthesisUtterance(result.replace('_', ' '));
+synth.speak(utterThis);
+
+}
