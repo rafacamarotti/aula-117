e2ab293feee76505e21c3cb8e221f9d493e2b713
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-03-21 20:44:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function setup(){
     canvas=createCanvas(280, 280);
-    canvas = center();
+    canvas.center();
     background("white");
     canvas.mouseReleased(classifyCanvas);
     synth = window.speechSynthesis;
